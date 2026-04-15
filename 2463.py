607c2943e0b5daf9f26eefad1ca3c357e0223a1a
aminul2611 <aminul35-336@diu.edu.bd>
2026-04-15 11:10:04 +0600

    update

--- 2463.py
+++ 2463.py
@@ -1,46 +1,37 @@
-class Solution(object):
+import sys
+
+class Solution:
     def minimumTotalDistance(self, robot, factory):
         robot.sort()
         factory.sort()
 
-        from functools import lru_cache
+        n = len(robot)
+        m = len(factory)
 
-        @lru_cache(None)
+        sys.setrecursionlimit(10**7)
+
+        memo = {}
+
         def dp(i, j):
-            if i == len(robot):
+            if i == n:
                 return 0
-            
-            if j == len(factory):
+            if j == m:
                 return float('inf')
+
+            if (i, j) in memo:
+                return memo[(i, j)]
 
             res = dp(i, j + 1)
 
             pos, limit = factory[j]
+
             total = 0
 
-            for k in range(1, limit + 1):
-                if i + k - 1 >= len(robot):
-                    break
-
+            for k in range(1, min(limit, n - i) + 1):
                 total += abs(robot[i + k - 1] - pos)
                 res = min(res, total + dp(i + k, j + 1))
 
+            memo[(i, j)] = res
             return res
 
         return dp(0, 0)
-
-
-if __name__ == "__main__":
-    robot = list(map(int, input("Enter robots: ").split()))
-    
-    n = int(input("Number of factories: "))
-    factory = []
-
-    for _ in range(n):
-        pos, limit = map(int, input("Enter factory position and limit: ").split())
-        factory.append([pos, limit])
-
-    sol = Solution()
-    result = sol.minimumTotalDistance(robot, factory)
-
-    print("Minimum Distance:", result)
