db53d9137e43af106570d686ea3b3fa07a69280a
aminul2611 <aminul35-336@diu.edu.bd>
2026-04-18 09:47:52 +0600

    LeetCode Solve

--- 3783 Mirror Distance of an Integer .py
+++ 3783 Mirror Distance of an Integer .py
@@ -3,8 +3,3 @@ class Solution(object):
         rev = int(str(n)[::-1])
         return abs(n-rev)
     
-sol = Solution()
-
-while True:
-    n = int(input("Enter a number: "))
-    print(sol.mirrorDistance(n))
