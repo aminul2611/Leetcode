c8443982f975bcf6bc21aff5be32c5dae0aaffc5
aminul2611 <aminul35-336@diu.edu.bd>
2026-05-04 15:28:11 +0600

    Problem Solve

--- 9. palindrome number.py
+++ 9. palindrome number.py
@@ -10,3 +10,25 @@ class Solution(object):
             x //= 10
 
         return x == reversed_half or x == reversed_half // 10
+if __name__ == "__main__":
+    sol = Solution()
+
+    print(" exit")
+    while True: 
+        user_input = input("Enter an interger: ")
+
+        if user_input == 'exit':
+            print("Goodbye!")
+            break
+    
+        try:
+            number = int(user_input)
+
+            is_pali = sol.isPalindrome(number)
+
+            print(f"Input: x = {number}")
+            print(f"Output: {str(is_pali).lower()}")
+        except ValueError:
+            print("Invalid input! Please enter a valid integer.")
+
+
